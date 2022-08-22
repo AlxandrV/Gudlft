2cf4a675417bcbaec1834c56de314b8be51867d6
AlxandrV <a.vigneaud@gmail.com>
2022-08-22 17:54:17 +0200

    Add - add test login should mail error

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,4 +1,5 @@
 import server
+import pytest
 
 class TestLogin:
 
@@ -6,4 +7,16 @@ class TestLogin:
         mocker.patch.object(server, 'clubs', clubs)
         data = {'email': clubs[0]['email']}
         response = client.post('/showSummary', data=data, follow_redirects=True)
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize(
+        'email',
+        [
+           ({'email': 'unlisted@test.com'}),
+           ({'other': ''}),
+           ({'': ''}),
+           ()
+        ])
+    def test_login_should_mail_error(self, client, email):
+        response = client.post('/showSummary', data=email, follow_redirects=True)
         assert response.status_code == 200
