426bea43fce8d36cb0243e04d06689d8c541dded
AlxandrV <a.vigneaud@gmail.com>
2022-08-30 15:15:48 +0200

    Add - add test unit purchase more max places

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+import server
+import pytest
+from flask import template_rendered
+
+
+@pytest.fixture
+def captured_templates(app=server.app):
+    recorded = []
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
+@pytest.fixture
+def client():
+    with server.app.test_client() as client:
+        yield client
+
+@pytest.fixture
+def clubs():
+    return [
+        {
+            "name": "Test club",
+            "email": "test_club@test.com",
+            "points": "10"
+        }
+    ]
+
+@pytest.fixture
+def competitions():
+    return [
+        {
+            "name": "Test competition",
+            "date": "2022-12-10 08:55:23",
+            "numberOfPlaces": "20"
+        }
+    ]
---
+++ tests/unit/test_server.py
@@ -0,0 +1,21 @@
+import server
+import pytest
+
+class TestPurchasePlaces:
+
+    def test_purchase_max_places(self, mocker, client, clubs, competitions):
+        max_places = 8
+        mocker.patch.object(server, 'clubs', clubs)
+        mocker.patch.object(server, 'competitions', competitions)
+        mocker.patch.object(server, 'MAX_PLACES', max_places)
+        club = clubs[0]
+        competition = competitions[0]
+        data = {
+            'competition': competition['name'],
+            'club': club['name'],
+            'places': max_places + 1
+        }
+        response = client.post('/purchasePlaces', data=data, follow_redirects=True)
+        res_data = response.data.decode('utf-8')
+        assert f'Maximum number of places at reservation {max_places}!' in res_data
+        assert response.status_code == 200
