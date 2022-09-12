2b25a392f3db679d800a6f1d182597caa986f0c7
AlxandrV <a.vigneaud@gmail.com>
2022-09-12 22:54:56 +0200

    Fix - fix error booking unknow competition or club

--- server.py
+++ server.py
@@ -43,13 +43,13 @@ def showSummary():
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
+    try:
+        foundClub = [c for c in clubs if c['name'] == club][0]
+        foundCompetition = [c for c in competitions if c['name'] == competition][0]
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
-    else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+    except Exception:
+        return page_not_found()
+
 
 
 @app.route('/purchasePlaces',methods=['POST'])
